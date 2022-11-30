558a32cd0baf8cebb8a2d86854471a64dcbbd6a9
lattecoff <lattecoff@lattecoff.com>
2022-11-30 13:11:15 +0300

    Output number miltiple of 11.

---
+++ main.py
@@ -0,0 +1,6 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+for i in range(0, 10000)
+    if (i %11 == 0):
+        print(i)
