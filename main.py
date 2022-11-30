6c08301dc34983a708484e58711f5feebf8ba72c
lattecoff <lattecoff@lattecoff.com>
2022-11-30 13:18:10 +0300

    "Calc sum."

---
+++ main.py
@@ -0,0 +1,11 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+min = int(input("Enter min: "))
+max = int(input("Enter max: "))
+sum = 0
+
+for i in range (min, max):
+    sum += i
+
+print(sum)
