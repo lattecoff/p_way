5654d487f42fdf5439def8e11b96934c7c7dc789
lattecoff <lattecoff@lattecoff.com>
2022-12-02 17:09:09 +0300

    Added comments

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from random import randrange
 lenght = 10
 mean = 0;
 
+# Заполняем список положительными случайными числами.
 my_list = [randrange(0, 100) for i in range(lenght)]
 
+# Вычислем среднее значение значений списка.
 mean = sum(my_list) / len(my_list)
 
+# Находим элемнты которые меньше среднего значения списка.
 for i in my_list:
     if i < mean:
         print(i)
