21ace6250b87ced6ae6244a1a58fa527c5f2b1be
lattecoff <lattecoff@lattecoff.com>
2022-12-02 17:07:04 +0300

    Added comments

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 from random import randrange
 
 lenght = 100
-sum = 0;
+sum = 0
 
+# Заполняем список случайными элементами.
 my_list = [randrange(-100, 100) for i in range(lenght)]
 
+# Считаем сумму положительных четных элементов
 for i in my_list:
     if i > 0 and i %2 == 0:
         sum += i
